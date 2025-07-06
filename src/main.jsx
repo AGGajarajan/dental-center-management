f476d91f422490c9fbec0a2f3dcbd00a66a04604
Gaja Raj <aggajarajan@gmail.com>
2025-07-06 15:03:52 +0530

    fix the some changes

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './styles/tailwind.css'; 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/dental-center-management">
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // Use dynamic base depending on environment
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/dental-center-management/' : '/',
+ base: '/dental-center-management/',
   plugins: [react()]
 })
